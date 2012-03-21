b5f35eb9da2dbe35390caab551a40de037c2a39f
Benoît Suttor <bsuttor@cirb.irisnet.be>
2012-03-21 11:51:02 +0100

    first step tests

--- collective/checktranslated/symptoms.py
+++ collective/checktranslated/symptoms.py
@@ -30,14 +30,20 @@ class HasTranslatedFR(SymptomBase):
     def _update(self):
         site = getSite()
         languages = site.portal_languages.getAvailableLanguages().keys()
-    
         self.status, self.description = check_translated(self.context, languages, 'fr')
-
 
 
 def check_translated(context, languages, lang):
     current_lang = context.Language()
-    languages.remove(current_lang)
+    status=''
+    description=''
+    if current_lang:
+        languages.remove(current_lang)
+    else:
+        status = False
+        description = _(u"This is a neutral language object.")
+        return status, description
+
     if current_lang == lang:
         status = True
         description = _("This is the current language.")
@@ -48,18 +54,14 @@ def check_translated(context, languages, lang):
         status = False
         description = _(u"{0} is not installed on your site.".format(lang))
     else:
-        translate_context = ''
         if hasattr(context, 'getTranslation'):
             translate_context = context.getTranslation(lang)
-        else:
-            status = False
-            description = _(u"This is a neutral language object.")
-        if translate_context is None:
-            status = False
-            description = _(u"There is no {0} translation".format(lang))
-        else:
-            status = True
-            description = _(u"All right.")
+            if translate_context is None:
+                status = False
+                description = _(u"There is no {0} translation".format(lang))
+            else:
+                status = True
+                description = _(u"All right.")
 
     return status, description
 
--- collective/checktranslated/tests/test_translated.py
+++ collective/checktranslated/tests/test_translated.py
@@ -2,35 +2,22 @@
 import unittest2 as unittest
 
 from collective.checktranslated.symptoms import check_translated
-from collective.checktranslated.testing import CHECKTRANSLATED
-
+from collective.checktranslated.testing import CHECKTRANSLATED_INTEGRATION
+from collective.checktranslated.testing import CHECKTRANSLATED_FUNCTIONAL
 from Products.LinguaPlone.public import BaseContent
 from Products.LinguaPlone.public import registerType
 from Products.Archetypes.Schema.factory import instanceSchemaFactory
 from Products.LinguaPlone.utils import LocateTranslation
-from Products.LinguaPlone.interfaces import ILocateTranslation
-from Products.LinguaPlone.interfaces import ITranslatable
 
 from zope.component import testing
 from zope.component import provideAdapter
 
-class testBaseContent(BaseContent):
-    '''
-    def __init__(self):
-        super(testBaseObject, self).__init__()
-        self._language = None
-
-    def Language(self):
-        return self._language
-
-    def UID(self):
-        return None
-    '''
-
-registerType(testBaseContent)
+from plone.app.testing import TEST_USER_NAME, TEST_USER_ID
+from plone.app.testing import setRoles
 
 class TestTranslated(unittest.TestCase):
     site_languages = ['fr', 'en', 'nl']
+    '''
     def setUp(self):
         testing.setUp(self)
         provideAdapter(instanceSchemaFactory)
@@ -40,34 +27,46 @@ class TestTranslated(unittest.TestCase):
     
     def tearDown(self):
         testing.tearDown(self)
-
+    '''
+    layer = CHECKTRANSLATED_INTEGRATION
     def test_translated_in_nl(self):
-        fr_object = testBaseContent('fr_object')
-        fr_object.setLanguage('fr')
+        site = self.layer['portal']
+        setRoles(site, TEST_USER_ID, ('Manager',))
+        site.invokeFactory(type_name="Folder", id="fr_object", language="fr")
+        fr_object = site.fr_object
         status, description  = check_translated(fr_object, self.site_languages, 'nl')
         self.assertFalse(status)
 
     def test_neutral(self):
-        neutral_object = testBaseContent('neutral_object')
+        site = self.layer['portal']
+        setRoles(site, TEST_USER_ID, ('Manager',))
+        site.invokeFactory(type_name="Folder", id="neutral_object", language="")
+        neutral_object = site.neutral_object
         status, description  = check_translated(neutral_object, self.site_languages, 'nl')
         self.assertFalse(status)
     
     def test_current_language(self):
-        fr_object = testBaseContent('fr_object')
-        fr_object.setLanguage('fr')
+        site = self.layer['portal']
+        setRoles(site, TEST_USER_ID, ('Manager',))
+        site.invokeFactory(type_name="Folder", id="object_current_test", language="fr")
+        fr_object = site.object_current_test
+        
         status, description  = check_translated(fr_object, self.site_languages, 'fr')
-        self.assertFalse(status)
-
+        self.assertTrue(status)
+    '''
     def test_translated(self):
-        fr_object = testBaseContent('fr_object')
-        fr_object.setLanguage('fr')
-        nl_object = testBaseContent('nl_object')
-        nl_object.setLanguage('nl')
+        site = self.layer['portal']
+        setRoles(site, TEST_USER_ID, ('Manager',))
+        site.invokeFactory(type_name="Folder", id="object_translated_test", language="fr")
+        fr_object = site.object_translated_test
+        
 
+        site.invokeFactory(type_name="Folder", id="nl_object", language="nl")
+        nl_object = site.nl_object
         fr_object.addTranslation(nl_object)
         status, description  = check_translated(fr_object, self.site_languages, 'nl')
         self.assertTrue(status)
         
-        status, description  = check_translated(nl_object, self.site_languages, 'fr')
-        self.assertTrue(status)
-
+        #status, description  = check_translated(nl_object, self.site_languages, 'fr')
+        #self.assertTrue(status)
+    '''
